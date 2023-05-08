400e009b21fce9d4b0d5c9e48a58606eafe38f81
Helton-Pereira <helton.bfsm@gmail.com>
2023-05-08 17:45:40 -0300

    Req 08

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,27 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    occurrences = list()
+
+    for item in instance.data():
+        count = 0
+        for content in item['linhas_do_arquivo']:
+            count += 1
+            if word.lower() in content.lower():
+                occurrences.append(
+                    {
+                        'linha': count,
+                        'conteudo': content
+                    })
+        if len(occurrences) < 1:
+            return []
+
+        result.append(
+            {
+                'palavra': word,
+                'arquivo': item['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            }
+        )
+    return result
