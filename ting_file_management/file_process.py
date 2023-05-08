35e4b9bffea774149f5bb11acd4cea7cae8c3da9
Helton-Pereira <helton.bfsm@gmail.com>
2023-05-08 15:48:20 -0300

    Req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_path = ''
+
+    for index in range(instance.__len__()):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            file_path = instance.search(index)
+
+    if file_path == '':
+        text = txt_importer(path_file)
+        lines = len(text)
+        result = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': lines,
+            'linhas_do_arquivo': text,
+        }
+        instance.enqueue(result)
+        print(f'{result}', file=sys.stdout)
 
 
 def remove(instance):
